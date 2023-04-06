ae98d248f5026c5ec4015519bda11e6ed8777065
nemanjastojanovich <109088925+nemanjastojanovich@users.noreply.github.com>
2023-04-06 12:13:11 +0200

    Inserted header and footer

---
+++ src/components/FooterApp.vue
@@ -0,0 +1,50 @@
+<template>
+    <footer>
+        <h7>Pratite me</h7>
+        <div class="social-links">
+            <a href="https://www.facebook.com/nemanja.stojanovic.7568 " target="_blank"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://www.linkedin.com/in/nemanja-stojanovic-a85ba463/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+            <a href="https://www.youtube.com/channel/UCMYh36toRibxsITk-oSkF9w" target="_blank"><i class="fab fa-youtube"></i></a>
+        </div>
+        <div class="footerCopyright">
+          <small>Copyright &copy; Nemanja Stojanović</small>
+        </div>
+    </footer>
+</template>
+<style lang="scss" scoped>
+footer {
+    text-align: center;
+    background-color: #3772b1;
+    padding: 10px;
+    margin: 40px auto 0;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    .social-links {
+      a {
+        margin: 5px;
+        color: rgb(29, 29, 141);
+      }
+    }
+  }
+  footer h7 {
+    color: rgb(29, 29, 141);
+  }
+  footer h7::after {
+    content: "";
+    width: 55px;
+    height: 1px;
+    display: block;
+    background-color: rgb(29, 29, 141);;
+    margin: 0 auto;
+    margin-top: 5px;
+    margin-bottom: 5px;
+  }
+  footer .footerCopyright {
+    text-align: center;
+    padding: 15px 0;
+    border-top: 1px solid rgb(29, 29, 141);;
+    margin-top: 10px;
+    color: rgb(0, 0, 128);
+  } 
+</style>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,98 @@
+<script>
+export default {
+  data() {
+    return {
+      options: [
+        { label: 'SignIn form', value: 'option1' },
+        { label: 'Vremenska prognoza', value: 'option3' },
+        { label: 'Todo list', value: 'option3' },
+      ],
+      selectedOption: 'option1',
+    };
+  },
+};
+</script>
+
+
+<template>
+  <header>
+    <nav>
+      <img src="../assets/logo/favicon.jpg" class="profile-image" alt="Logo Nemanja Stojanović" />
+      <ul>
+        <li>
+          <a aria-current="page" href="#">Kontakt</a>
+        </li>
+        <li>
+          <a href="#">Obrazovanje i edukacija</a>
+        </li>
+        <li>
+          <div class="dropdown">
+            
+            <select id="dropdown" v-model="selectedOption">
+              <option value="">Projekti</option>
+              <option v-for="option in options" :value="option.value">{{ option.label }}</option>
+            </select>
+          </div>
+        </li> 
+      </ul>
+    </nav>
+  </header>
+</template>
+<style lang="scss" scoped>
+header {
+  position: fixed;
+  width: 100%;
+  background-color: #3772b1;
+  padding: 10px;
+  -webkit-box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.41);
+  -moz-box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.41);
+  box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.41);
+
+
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+
+    ul {
+      display: flex;
+      list-style: none;
+      justify-content: space-around;
+      gap: 10px;
+      li {
+        span {
+          color: white;
+        }
+        a {
+          display: inline-block;
+          color: white;
+          text-decoration: none;
+        }
+      }
+    }
+  }
+}
+li:hover {
+  cursor: pointer;
+}
+img.profile-img {
+  height: 20px;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+  select {
+    background-color: #3772b1;
+    border: none;
+    color: white;
+    font-family: 'Ubuntu', serif;
+    font-size: 16px;
+  }
+}
+.dropdown:hover {
+  display: flex;
+  color: blue;
+}
+</style>
