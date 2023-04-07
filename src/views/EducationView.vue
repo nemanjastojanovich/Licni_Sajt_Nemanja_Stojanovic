d584ee198296ab5418378f135e9bec11f5021943
nemanjastojanovich <109088925+nemanjastojanovich@users.noreply.github.com>
2023-04-07 15:16:57 +0200

    Created education page

---
+++ src/views/EducationView.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="education">
+    <h1>Obrazovanje i edukacija:</h1>
+    <h2>Fakultet:</h2>
+    <ul>
+      <li>MBS Univerzitet - Diplomirani menadžer u uslužnom biznisu</li>
+      <li>Visoka škola poslovnih studija Univerziteta u Beogradu</li>
+      <li>Inženjer informatike</li>
+    </ul>
+    <h2>Srednja škola:</h2>
+    <ul>
+      <li>Politehnička akademija</li>
+      <li>Tehničar za robotiku</li>
+    </ul>
+      <h2>Završene onuke i kursevi:</h2>
+      <ul>
+        <li>
+            Code by Comtrade - Frontend Developer
+        </li>
+        <li>
+            Licenca za posredovanje i zastupanje u osiguranju
+        </li>
+        <li>
+            Obuka za ručnu proveru autentičnosti i obradu gotovog novca
+        </li>
+        <li>
+            Prodajne veštine - Alterna international by Brian Tracy
+        </li>
+        <li>
+            Liderske veštine - Alterna international by Brian Tracy
+        </li>
+      </ul>
+      <h2>Jezici:</h2>
+      <ul>
+        <li>
+            Engleski: Napredni nivo
+        </li>
+        <li>
+            Ruski: Osnovni nivo
+        </li>
+      </ul>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.education {
+  padding-top: 120px;
+  padding-bottom: 50px;
+    margin: 0 auto;
+    width: 80%;
+    h1 {
+        text-align: center;
+        padding-bottom: 50px;
+        color: rgb(29, 29, 141);
+        letter-spacing: 2px;
+        font-size: 30px;
+        font-weight: 700;
+    }
+    h2 {    
+        color: rgb(29, 29, 141);
+        letter-spacing: 2px;
+        font-size: 25px;
+        font-weight: 700;
+        line-height: 50px;
+    }
+    ul {
+       
+        li {
+            color: rgb(29, 29, 141);
+            letter-spacing: 2px;
+            font-size: 20;
+            font-weight: 400;
+            line-height: 25px;
+        }
+    }
+    
+}
+</style>
